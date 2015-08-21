846875635352a57e5968fc6eab4889002f1c5ca0
Kryptyk Fysh <kryptykfysh@kryptykfysh.co.uk>
2015-08-21 16:26:10 +1000

    Authentication only through Google OAuth.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+  devise :rememberable, :trackable, :omniauthable,
+    omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(access_token)
     Rails.logger.debug access_token.inspect
